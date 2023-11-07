54b32974c3838c6874f364cc23df7206ab1637ef
sam <manor@hey.com>
2023-11-07 14:17:44 -0500

    feat: add dependencies, row+stitch storybook files

---
+++ src/components/Row/Row.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+interface RowProps {}
+
+export const Row: FC<RowProps> = () => {
+    return <>Row component!</>;
+};
---
+++ src/components/Stitch/Stitch.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+interface StitchProps {}
+
+export const Stitch: FC<StitchProps> = () => {
+    return <></>;
+};
