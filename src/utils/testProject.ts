663bf49f4ff90a9eff869431786a839596ce1ece
sam <manor@hey.com>
2024-01-24 11:48:05 -0500

    move testProject to its own file

---
+++ src/utils/testProject.ts
@@ -0,0 +1,144 @@
+export const testProject = {
+	// projectName: "test project",
+	currentProjectRow: 1,
+	blocks: [
+		{
+			currentBlockRow: 1,
+			stitches: [
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+			],
+		},
+		{
+			currentBlockRow: 1,
+			stitches: [
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+			],
+		},
+		{
+			currentBlockRow: 1,
+			stitches: [
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+			],
+		},
+		{
+			currentBlockRow: 1,
+			stitches: [
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+			],
+		},
+		{
+			currentBlockRow: 1,
+			stitches: [
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+				[
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+					{ name: "knit", abbreviation: "k", symbol: "*", description: "knit 1", width: 1 },
+					{ name: "purl", abbreviation: "p", symbol: "-", description: "purl 1", width: 1 },
+				],
+			],
+		},
+	],
+};
