5802fbe215f820071cdd5a095581a94428db069c
sam <manor@hey.com>
2024-02-15 19:48:30 -0500

    fix big with adding a new block, refactor project component

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -6,22 +6,18 @@ import { EditOutlined, DeleteOutlined, AddOutlined, CancelOutlined } from "@mui/
 import { BlockEditor } from "../BlockEditor";
 import { deleteBlock, addBlock } from "../../reducers/projectReducer";
 
-export interface ProjectProps {
-	projectName?: string;
-	// blocks?: BlockProps[];
-}
+export interface ProjectProps {}
 
 /**
  * A project; made up of many blocks.
- * @param blocks The blocks of the project.
  */
-export const Project: FC<ProjectProps> = ({}) => {
+export const Project: FC<ProjectProps> = () => {
 	const currentRow = useSelector((state: any) => state.projects.currentRow);
 	const currentMode = useSelector((state: any) => state.workspace.mode);
 	const blocks = useSelector((state: any) => state.projects.project.blocks);
 	const [showBlockEditor, setShowBlockEditor] = useState(false);
 	const [showBlockMenu, setShowBlockMenu] = useState(false);
-	const [currentDraftBlock, setCurrentDraftBlock] = useState(0);
+	const [currentDraftBlock, setCurrentDraftBlock] = useState(-1);
 
 	const dispatch = useDispatch();
 
@@ -44,84 +40,91 @@ export const Project: FC<ProjectProps> = ({}) => {
 		setCurrentDraftBlock(blockIndex);
 	};
 
-	const handleAddNewBlock = () => {
-		dispatch(addBlock({ blockIndex: blocks.length }));
+	const handleAddNewBlock = (position: "start" | "end") => {
+		dispatch(addBlock({ blockIndex: position === "start" ? 0 : blocks.length }));
 		setShowBlockMenu(false);
-		setCurrentDraftBlock(blocks.length);
 		setShowBlockEditor(true);
 	};
 
-	const addBlockMenu = (
-		<ClickAwayListener onClickAway={() => setShowBlockMenu(false)}>
-			<Grid container sx={{ position: "absolute" }}>
-				<IconButton onClick={() => setShowBlockMenu(false)}>
-					<CancelOutlined />
-				</IconButton>
+	const project = blocks.map((block: BlockProps, i: number) => {
+		return (
+			<Box key={i} sx={{ display: "flex", alignItems: "flex-end" }}>
+				{currentMode === "edit" ? (
+					<Grid container position={"absolute"}>
+						<IconButton onClick={() => handleEdit(i)}>
+							<EditOutlined />
+						</IconButton>
+						<IconButton onClick={() => dispatch(deleteBlock({ blockIndex: i }))}>
+							<DeleteOutlined />
+						</IconButton>
+					</Grid>
+				) : null}
+				<Block
+					stitches={block.stitches}
+					blockName={block.blockName}
+					index={i}
+					tallestBlockIndex={getTallestBlock()}
+				/>
+			</Box>
+		);
+	});
+
+	if (currentMode === "edit") {
+		return (
+			<Grid container sx={{ background: "gray", border: "2px solid black", justifyContent: "center", gap: 2 }}>
+				{showBlockEditor ? (
+					<BlockEditor blockIndex={currentDraftBlock} closeEditor={() => setShowBlockEditor(false)} />
+				) : null}
 				<Grid item>
-					<Button onClick={() => handleAddNewBlock()}>
-						<Typography>create new block</Typography>
-					</Button>
+					<IconButton
+						onClick={() => {
+							setShowBlockMenu(true);
+							setCurrentDraftBlock(0);
+						}}
+					>
+						<AddOutlined />
+					</IconButton>
 				</Grid>
+				{showBlockMenu ? (
+					<ClickAwayListener onClickAway={() => setShowBlockMenu(false)}>
+						<Grid container sx={{ position: "absolute" }}>
+							<IconButton onClick={() => setShowBlockMenu(false)}>
+								<CancelOutlined />
+							</IconButton>
+							<Grid item>
+								<Button onClick={() => handleAddNewBlock(currentDraftBlock === 0 ? "start" : "end")}>
+									<Typography>create new block</Typography>
+								</Button>
+							</Grid>
+							<Grid item>
+								<Button>
+									<Typography>add new instance of an existing block</Typography>
+								</Button>
+							</Grid>
+						</Grid>
+					</ClickAwayListener>
+				) : null}
+				{project}
 				<Grid item>
-					<Button>
-						<Typography>add new instance of an existing block</Typography>
-					</Button>
+					<IconButton
+						onClick={() => {
+							setShowBlockMenu(true);
+							setCurrentDraftBlock(blocks.length);
+						}}
+					>
+						<AddOutlined />
+					</IconButton>
 				</Grid>
 			</Grid>
-		</ClickAwayListener>
-	);
+		);
+	}
 
-	return (
-		<Grid
-			container
-			sx={{
-				background: "gray",
-				border: "2px solid black",
-				justifyContent: "center",
-				gap: 2,
-			}}
-		>
-			{currentMode === "edit" && showBlockEditor ? (
-				<BlockEditor blockIndex={currentDraftBlock} closeEditor={() => setShowBlockEditor(false)} />
-			) : null}
-			{currentMode === "chart" ? <Typography variant="h6">current row: {currentRow}</Typography> : null}
-			{currentMode === "edit" ? (
-				<Grid item sx={{}}>
-					<IconButton onClick={() => dispatch(addBlock({ blockIndex: 0 }))}>
-						<AddOutlined />
-					</IconButton>
-				</Grid>
-			) : null}
-			{currentMode === "edit" && showBlockMenu ? addBlockMenu : null}
-			{blocks.map((block: BlockProps, i: number) => {
-				return (
-					<Box key={i} sx={{ display: "flex", alignItems: "flex-end" }}>
-						{currentMode === "edit" ? (
-							<Grid container position={"absolute"}>
-								<IconButton onClick={() => handleEdit(i)}>
-									<EditOutlined />
-								</IconButton>
-								<IconButton onClick={() => dispatch(deleteBlock({ blockIndex: i }))}>
-									<DeleteOutlined />
-								</IconButton>
-							</Grid>
-						) : null}
-						<Block
-							stitches={block.stitches}
-							blockName={block.blockName}
-							index={i}
-							tallestBlockIndex={getTallestBlock()}
-						/>
-					</Box>
-				);
-			})}
-			{currentMode === "edit" ? (
-				<Grid item sx={{}}>
-					<IconButton onClick={() => setShowBlockMenu(true)}>
-						<AddOutlined />
-					</IconButton>
-				</Grid>
-			) : null}
-		</Grid>
-	);
+	if (currentMode === "chart") {
+		return (
+			<Grid container sx={{ background: "gray", border: "2px solid black", justifyContent: "center", gap: 2 }}>
+				<Typography variant="h6">current row: {currentRow}</Typography>
+				{project}
+			</Grid>
+		);
+	}
 };
