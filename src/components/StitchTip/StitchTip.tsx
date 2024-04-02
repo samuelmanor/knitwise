4ab0b8e66b5e5d4963d12114d7c360feb2381301
sam <manor@hey.com>
2024-04-02 17:56:05 -0400

    add stitchtip css

--- src/components/StitchTip/StitchTip.tsx
+++ src/components/StitchTip/StitchTip.tsx
@@ -1,4 +1,4 @@
-import { Grid, Tooltip, Typography } from "@mui/material";
+import { Grid, Tooltip, Typography, useTheme } from "@mui/material";
 import { FC } from "react";
 
 interface StitchTipProps {
@@ -10,18 +10,47 @@ interface StitchTipProps {
 /**
  * A tooltip that displays the name, abbreviation, and description of a stitch.
  * @param children The content to be wrapped by the tooltip.
+ * @param name The name of the stitch.
+ * @param description A description of the stitch that describes how to work it.
  */
 export const StitchTip: FC<StitchTipProps> = ({ children, name, description }) => {
+	const theme = useTheme();
+
 	const tooltipText = (
 		<Grid container direction="column">
-			<Typography variant="h6">{name}</Typography>
-			{/* <Typography variant="body1">{abbreviation}</Typography> */}
+			<Typography
+				variant="h6"
+				sx={{
+					fontWeight: "bold",
+					width: "fit-content",
+					mb: 1,
+				}}
+			>
+				{name}
+			</Typography>
 			<Typography variant="body2">{description}</Typography>
 		</Grid>
 	);
 
 	return (
-		<Tooltip title={tooltipText} arrow componentsProps={{ tooltip: { sx: { width: "200px" } } }}>
+		<Tooltip
+			title={tooltipText}
+			arrow
+			componentsProps={{
+				tooltip: {
+					sx: {
+						"fontSize": "15px",
+						"maxWidth": "200px",
+						"p": 1,
+						"backgroundColor": theme.palette.primary.main,
+						"letterSpacing": "1px",
+						"& .MuiTooltip-arrow": {
+							color: theme.palette.primary.main,
+						},
+					},
+				},
+			}}
+		>
 			<Grid container>{children}</Grid>
 		</Tooltip>
 	);
