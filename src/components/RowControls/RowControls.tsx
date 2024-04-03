576f98c90e7ca17dcf25f885f6e0c52bab3f9676
sam <manor@hey.com>
2024-04-03 17:59:07 -0400

    move row controls to its own component, fix workspace backgroundimage overflowy bug

--- src/components/RowControls/RowControls.tsx
+++ src/components/RowControls/RowControls.tsx
@@ -1,55 +1,101 @@
 import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from "@mui/icons-material";
 import { Grid, IconButton, Tooltip, Typography, useTheme } from "@mui/material";
 import { FC } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { nextRow, prevRow } from "../../reducers/projectReducer";
 
 interface RowControlsProps {}
 
+/**
+ * Contains the controls for navigating from row to row, and the current row number.
+ */
 export const RowControls: FC<RowControlsProps> = () => {
 	const currentRow = useSelector((state: any) => state.projects.currentRow);
+
+	const dispatch = useDispatch();
 	const theme = useTheme();
 
 	return (
-		<Grid item sx={{ display: "flex", flexDirection: "row", alignItems: "baseline", gap: 2 }}>
-			{currentRow !== 1 ? (
+		<Grid container sx={{ position: "absolute", justifyContent: "center" }}>
+			<Grid
+				item
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+					gap: 2,
+				}}
+			>
+				{currentRow !== 1 ? (
+					<Tooltip
+						title="previous row"
+						placement="top-end"
+						arrow
+						slotProps={{ popper: { modifiers: [{ name: "offset", options: { offset: [0, 10] } }] } }}
+						componentsProps={{
+							tooltip: {
+								sx: {
+									"fontSize": "15px",
+									"maxWidth": "200px",
+									"p": 1,
+									"backgroundColor": theme.palette.primary.main,
+									"letterSpacing": "1px",
+									"fontWeight": "bold",
+									"& .MuiTooltip-arrow": {
+										color: theme.palette.primary.main,
+									},
+								},
+							},
+						}}
+					>
+						<IconButton
+							onClick={() => dispatch(prevRow())}
+							sx={{ color: theme.palette.text.secondary }}
+							size="large"
+						>
+							<ArrowBackIosNewOutlined />
+						</IconButton>
+					</Tooltip>
+				) : (
+					<IconButton disabled size="large">
+						<ArrowBackIosNewOutlined />
+					</IconButton>
+				)}
+
+				<Typography variant="h5" sx={{ color: theme.palette.text.secondary, fontWeight: "bold" }}>
+					{currentRow}
+				</Typography>
+
 				<Tooltip
-					title={<Typography variant="body1">previous row</Typography>}
-					placement="top-end"
-					slotProps={{ popper: { modifiers: [{ name: "offset", options: { offset: [0, -10] } }] } }}
-					sx={{ fontSize: "15px" }}
+					title="next row"
+					placement="top-start"
+					arrow
+					slotProps={{ popper: { modifiers: [{ name: "offset", options: { offset: [0, 10] } }] } }}
+					componentsProps={{
+						tooltip: {
+							sx: {
+								"fontSize": "15px",
+								"maxWidth": "200px",
+								"p": 1,
+								"backgroundColor": theme.palette.primary.main,
+								"letterSpacing": "1px",
+								"fontWeight": "bold",
+								"& .MuiTooltip-arrow": {
+									color: theme.palette.primary.main,
+								},
+							},
+						},
+					}}
 				>
 					<IconButton
-						// onClick={() => dispatch(prevRow())}
+						onClick={() => dispatch(nextRow())}
 						sx={{ color: theme.palette.text.secondary }}
 						size="large"
 					>
-						<ArrowBackIosNewOutlined />
+						<ArrowForwardIosOutlined />
 					</IconButton>
 				</Tooltip>
-			) : (
-				<IconButton disabled size="large">
-					<ArrowBackIosNewOutlined />
-				</IconButton>
-			)}
-
-			<Typography variant="h5" fontFamily={"Arial"}>
-				{currentRow}
-			</Typography>
-
-			<Tooltip
-				title={<Typography variant="body1">next row</Typography>}
-				placement="top-start"
-				slotProps={{ popper: { modifiers: [{ name: "offset", options: { offset: [0, -10] } }] } }}
-				sx={{ fontSize: "15px" }}
-			>
-				<IconButton
-					// onClick={() => dispatch(nextRow())}
-					sx={{ color: theme.palette.text.secondary }}
-					size="large"
-				>
-					<ArrowForwardIosOutlined />
-				</IconButton>
-			</Tooltip>
+			</Grid>
 		</Grid>
 	);
 };
--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,7 +2,6 @@ import { FC, useRef, useState } from "react";
 import { Button, ClickAwayListener, Grid, IconButton, TextField, Typography, useTheme } from "@mui/material";
 import { Project } from "../Project";
 import { useDispatch, useSelector } from "react-redux";
-import { nextRow, prevRow, resetProject } from "../../reducers/projectReducer.js";
 import { setMode } from "../../reducers/workspaceReducer.js";
 import { editProjectName } from "../../reducers/workspaceReducer.js";
 import { CloseOutlined, EditOutlined, SaveOutlined } from "@mui/icons-material";
@@ -16,8 +15,6 @@ interface WorkspaceProps {}
 export const Workspace: FC<WorkspaceProps> = () => {
 	const currentMode = useSelector((state: any) => state.workspace.mode);
 	const currentProject = useSelector((state: any) => state.workspace.projects[state.workspace.currentProjectId]);
-	const [showEditWarning, setShowEditWarning] = useState(false);
-	// const [showSettingsMenu, setShowSettingsMenu] = useState(false);
 	const [showProjectNameEditor, setShowProjectNameEditor] = useState(false);
 	const [projectNameDraft, setProjectNameDraft] = useState(currentProject.projectName);
 	const projectMenuRef = useRef<HTMLDivElement>(null);
@@ -27,30 +24,6 @@ export const Workspace: FC<WorkspaceProps> = () => {
 
 	// make no project found component?
 	// if (!project) return null;
-
-	// const selectView = ( // for selecting which project to work on?
-	// 	<Grid container>
-	// 		available projects:
-	// 		<Grid item>
-	// 			{projects.map((project: any) => {
-	// 				return (
-	// 					<Grid item>
-	// 						<Button onClick={() => dispatch(setMode("chart"))}>{project.projectName}</Button>
-	// 					</Grid>
-	// 				);
-	// 			})}
-	// 		</Grid>
-	// 	</Grid>
-	// );
-
-	/**
-	 * Handles the edit button click; sets the mode to "edit", resets the project, and dismisses the warning.
-	 */
-	const handleEdit = () => {
-		dispatch(setMode("edit"));
-		dispatch(resetProject());
-		setShowEditWarning(false);
-	};
 
 	if (currentMode === "chart") {
 		return (
@@ -63,9 +36,8 @@ export const Workspace: FC<WorkspaceProps> = () => {
 						minWidth: "fit-content",
 						justifyContent: "center",
 						alignItems: "end",
-						backgroundImage:
-							// "linear-gradient(to right, #f6f2df, #f5f1dc, #f5f0d9, #f4efd6, #f3eed3, #f2edd0, #f2ebce, #f1eacb, #f0e9c8, #efe8c5, #eee6c2, #ede5bf)",
-							theme.palette.background.default,
+						backgroundImage: theme.palette.background.default,
+						overflowY: "scroll",
 					}}
 				>
 					<Grid container sx={{ justifyContent: "center" }}>
