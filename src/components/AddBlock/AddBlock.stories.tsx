903eca5a27eff2718d06c3672542ffbf262e2637
sam <manor@hey.com>
2024-09-12 14:31:04 -0500

    add themeprovider to story decorators

--- src/components/AddBlock/AddBlock.stories.tsx
+++ src/components/AddBlock/AddBlock.stories.tsx
@@ -2,36 +2,42 @@ import { Meta, StoryObj } from "@storybook/react";
 
 import { expect } from "@storybook/jest";
 import { within } from "@storybook/testing-library";
-
 import { AddBlock } from "./AddBlock";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { lightTheme } from "../../theme";
+import { Provider } from "react-redux";
+import { testProject } from "../../utils/testProject";
+import { usePreloadedState } from "../../reducers/store";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof AddBlock> = {
 	title: "AddBlock",
 	component: AddBlock,
 	parameters: {
-		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
 		layout: "centered",
 	},
-	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
 	tags: ["autodocs"],
-	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-	argTypes: {
-		// backgroundColor: { control: "color" },
-	},
+	decorators: [
+		Story => (
+			<ThemeProvider theme={createTheme(lightTheme)}>
+				<Provider
+					store={usePreloadedState({
+						project: testProject,
+					})}
+				>
+					<Story />
+				</Provider>
+			</ThemeProvider>
+		),
+	],
 };
 
 export default meta;
 type Story = StoryObj<typeof AddBlock>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
 	play: async ({ canvasElement }) => {
 		const canvas = within(canvasElement);
 		const element = canvas.getByText(/AddBlock/i);
 		expect(element).toBeTruthy();
-	},
-	args: {
-		// label: "AddBlock",
 	},
 };
