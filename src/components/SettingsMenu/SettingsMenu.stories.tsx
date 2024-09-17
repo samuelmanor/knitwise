ad7d3cd18465312c5fe088508d267c674d04adfb
sam <manor@hey.com>
2024-09-17 13:13:35 -0500

    update story test + info

--- src/components/SettingsMenu/SettingsMenu.stories.tsx
+++ src/components/SettingsMenu/SettingsMenu.stories.tsx
@@ -1,28 +1,19 @@
 import { Meta, StoryObj } from "@storybook/react";
-
 import { expect } from "@storybook/jest";
 import { within } from "@storybook/testing-library";
-
 import { SettingsMenu } from "./SettingsMenu";
 import { Provider } from "react-redux";
 import store from "./../../reducers/store";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { lightTheme } from "../../theme";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof SettingsMenu> = {
 	title: "SettingsMenu",
 	component: SettingsMenu,
 	parameters: {
-		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
 		layout: "centered",
 	},
-	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
 	tags: ["autodocs"],
-	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-	argTypes: {
-		// backgroundColor: { control: "color" },
-	},
 	decorators: [
 		Story => (
 			<ThemeProvider theme={createTheme(lightTheme)}>
@@ -37,14 +28,10 @@ const meta: Meta<typeof SettingsMenu> = {
 export default meta;
 type Story = StoryObj<typeof SettingsMenu>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
-	play: async ({ canvasElement }) => {
+	play: async ({ canvasElement, step }) => {
 		const canvas = within(canvasElement);
-		const element = canvas.getByText(/SettingsMenu/i);
+		const element = canvas.getByText(/Settings/i);
 		expect(element).toBeTruthy();
-	},
-	args: {
-		// label: "SettingsMenu",
 	},
 };
